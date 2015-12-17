1cdba07f9e8864bf3c6cb19c94264cb046ee3eca
vagrant <vagrant@precise32.(none)>
2015-12-17 22:08:40 +0000

    email send out test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,19 @@ Cph::Application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+  
+config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_PASSWORD']
+  }
+
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -14,7 +27,7 @@ Cph::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
