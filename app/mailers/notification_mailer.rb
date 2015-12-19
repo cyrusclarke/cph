963211b59068ff30dc06671719c78408a97d720f
vagrant <vagrant@precise32.(none)>
2015-12-19 12:46:11 +0000

    CSS Styling Homepage

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,7 +1,7 @@
 class NotificationMailer < ActionMailer::Base
   default from: "noreply@cphapp.com"
 
-  def comment_added
+  def comment_added(comment)
   		mail(to: "cyrusjclarke@gmail.com", subject: "A Comment was added to your post")
   	end 
 end
